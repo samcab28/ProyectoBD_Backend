e04b852aa2cb91cd87b0ba9d243d31d666779301
Luis <luisgerardourbsalz@gmail.com>
2024-05-24 10:41:34 -0600

    ModifcarAvanze
    Listo los modificar de persona, producto y sucursal

--- src/controllers/ctrProducto/ModProducto.js
+++ src/controllers/ctrProducto/ModProducto.js
@@ -1,4 +1,26 @@
-// Controlador para modificar un producto existente por ID
-export const updateProducto = (req, res) => {
-    res.send("Modificando producto");
-}
+import { getConnection } from "../../database/connection.js";
+import sql from 'mssql';
+
+// Controlador para modificar una Producto existente por ID
+export const updateProducto = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { campoModificar, valorNuevo } = req.body;
+
+        console.log("Datos recibidos:", campoModificar, valorNuevo); // Verificar los datos recibidos
+
+        const pool = await getConnection();
+
+        const result = await pool.request()
+            .input('IdProducto', sql.Int, id)
+            .input('CampoModificar', sql.NVarChar, campoModificar)
+            .input('ValorNuevo', sql.NVarChar, valorNuevo)
+            .query('EXEC ProductoModificar @IdProducto, @CampoModificar, @ValorNuevo');
+        
+        res.status(200).json({ message: 'Producto modificado exitosamente', data: result.recordset });
+    } catch (error) {
+        console.error("Error al modificar producto:", error);
+        res.status(500).send("Error al modificar producto");
+    }
+};
+

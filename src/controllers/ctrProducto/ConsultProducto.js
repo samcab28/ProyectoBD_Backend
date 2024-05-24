254d750a33cdce5a1e7c27a771def1ea43593e08
samcab28 <samircabrera2528@gmail.com>
2024-05-24 09:12:19 -0600

    crud producto

--- src/controllers/ctrProducto/ConsultProducto.js
+++ src/controllers/ctrProducto/ConsultProducto.js
@@ -6,7 +6,7 @@ export const getProductos = async (req, res) => {
         const pool = await getConnection();
         const result = await pool.request().query('exec ProductoConsulta');
         // Enviar una respuesta con el resultado de la consulta
-        res.send(result);
+        res.send(result.recordset);
     } catch(error){
         console.error("Error al obtener productos:", error);
         res.status(500).send("Error al obtener productos");
--- src/controllers/ctrProducto/DeleteProducto.js
+++ src/controllers/ctrProducto/DeleteProducto.js
@@ -1,4 +1,16 @@
 // Controlador para borrar un producto existente por ID
-export const deleteProducto = (req, res) => {
-    res.send("Borrando producto");
+import {getConnection} from "../../database/connection.js";
+
+export const deleteProducto = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const pool = await getConnection();
+        const result = await pool.request()
+            .input('id', id)
+            .query('exec ProductoEliminar @id');
+        res.send(result);
+    }catch(error){
+        console.error("Error al borrado de persona:", error);
+        res.status(500).send("Error al borrado de persona");
+    }
 }

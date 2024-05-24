1b99ccd93c7c461f9fa845e4cd1bc1b4e5c10fe0
samcab28 <samircabrera2528@gmail.com>
2024-05-23 22:26:10 -0600

    consulta persona

--- src/controllers/ctrPersona/ConsultaPersona.js
+++ src/controllers/ctrPersona/ConsultaPersona.js
@@ -1,9 +1,32 @@
+import {getConnection} from "../../database/connection.js";
+
 // Controlador para obtener todas las personas
-export const getPersona = (req, res) => {
-    res.send("obteniendo personas");
+export const getPersona = async (req, res) => {
+    try {
+        const pool = await getConnection();
+        const result = await pool.request().query('exec PersonaConsulta');
+        // Enviar una respuesta con el resultado de la consulta
+        res.send(result);
+    } catch (error) {
+        console.error("Error al obtener personas:", error);
+        res.status(500).send("Error al obtener personas");
+    }
 }
 
+
 // Controlador para obtener una sola persona por ID
-export const getPersonaById = (req, res) => {
-    res.send("obteniendo una sola persona");
+export const getPersonaById = async (req, res) => {
+    try {
+        const { id } = req.params; // Obtener el ID de los parámetros de la URL
+        const pool = await getConnection();
+        const result = await pool.request()
+            .input('id', id)
+            .query('exec PersonaConsulta @id'); // Utilizar una consulta parametrizada
+        // Enviar una respuesta con el resultado de la consulta
+        res.send(result.recordset);
+    } catch (error) {
+        console.error("Error al obtener una sola persona:", error);
+        res.status(500).send("Error al obtener una sola persona");
+    }
 }
+
--- src/controllers/ctrPersona/CreatePersona.js
+++ src/controllers/ctrPersona/CreatePersona.js
@@ -1,4 +1,12 @@
+import {getConnection} from "../../database/connection.js";
+
 // Controlador para crear una nueva persona
 export const createPersona = (req, res) => {
-    res.send("creando persona");
+    try{
+
+    } catch (error) {
+        console.error("Error al crear persona:", error);
+        res.status(500).send("Error al crear persona");
+    }
+
 }

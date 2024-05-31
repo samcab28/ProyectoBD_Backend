f727ff26f62361a969bc0866f0ec00e480790240
samcab28 <samircabrera2528@gmail.com>
2024-05-31 10:27:21 -0600

    crud sucursal

--- src/controllers/ctrPersona/ConsultaPersona.js
+++ src/controllers/ctrPersona/ConsultaPersona.js
@@ -31,5 +31,24 @@ export const getPersonaById = async (req, res) => {
 }
 
 
+//get persona segun tipo
+export const getPersonaByTipo = async (req, res) => {
+    try {
+        const { tipo } = req.params; // Obtener el ID de los parámetros de la URL
+        const pool = await getConnection();
+        const result = await pool.request()
+            .input('tipo', tipo)
+            .query('EXEC GetPersonasByTipoPersona @tipo'); // Utilizar una consulta parametrizada
+        // Enviar una respuesta con el resultado de la consulta
+        res.send(result.recordset);
+    } catch (error) {
+        console.error("Error al obtener la data:", error);
+        res.status(500).send("Error al obtener la data");
+    }
+}
+
+
+
+
 
 

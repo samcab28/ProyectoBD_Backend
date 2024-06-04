af99b6cd1773636f6ac1b928f910676240bc15e5
Luis <luisgerardourbsalz@gmail.com>
2024-06-03 22:56:04 -0600

    Update CarritoCrear.js

--- src/controllers/ctrCarrito/CarritoCrear.js
+++ src/controllers/ctrCarrito/CarritoCrear.js
@@ -3,15 +3,15 @@ import { getConnection } from "../../database/connection.js";
 // Controlador para crear una referencia carrito 
 export const createCarrito = async (req, res) => {
     try {
-        const { IdPersona, IdProducto, Cantidad } = req.body; // Obtener los datos del cuerpo de la petición
+        const { IdPersona, IdProducto, IdSucursal, Cantidad } = req.body;
         const pool = await getConnection();
         const result = await pool.request()
             .input('IdPersona', IdPersona)
             .input('IdProducto', IdProducto)
-            .input('Cantidad', Cantidad)
-            .query('EXEC CarritoCrear @IdPersona, @IdProducto, @Cantidad'); // Utilizar una consulta parametrizada
-        // Enviar una respuesta con el resultado de la consulta
-        res.json(result.recordset[0]);
+            .input('IdSucursal', IdSucursal)
+            .input('Cantidad', 1)
+            .query('exec CarritoCrear @IdPersona, @IdProducto, @IdSucursal, @Cantidad');
+        res.send(result.recordset[0]);
     } catch (error) {
         console.error("Error al agregar producto al carrito:", error);
         res.status(500).send("Error al agregar producto al carrito");

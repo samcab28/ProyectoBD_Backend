05ce551424ee609b35a06452da171e001a04ae08
samcab28 <samircabrera2528@gmail.com>
2024-06-11 16:32:43 -0600

    inicio de consultas

--- src/controllers/controllersHistorial/ctrHistorialLogin/ConsultaHistorialLogin.js
+++ src/controllers/controllersHistorial/ctrHistorialLogin/ConsultaHistorialLogin.js
@@ -29,3 +29,21 @@ export const getHistorialLoginById = async (req, res) => {
     }
 }
 
+
+
+//lista los ultimos logins falsos de ultimos dos minutos
+export const getHistorialLoginByHora = async (req, res) => {
+    try {
+        const { hora } = req.params; // Obtener la hora de los parámetros de la URL
+        const pool = await getConnection();
+        const result = await pool.request()
+            .input('hora_parametro', sql.DateTime, hora) // Ajustar el tipo de dato según el procedimiento almacenado
+            .query('EXEC HistorialLoginConteo @hora_parametro'); // Utilizar una consulta parametrizada con el nuevo procedimiento almacenado
+        // Enviar una respuesta con el resultado de la consulta
+        res.send(result.recordset);
+    } catch (error) {
+        console.error("Error al obtener los últimos logins falsos:", error);
+        res.status(500).send("Error al obtener los últimos logins falsos");
+    }
+}
+

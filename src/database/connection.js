9a12db0b990b42119ca102ae5069a9934a5183b7
Bonko24 <134968235+Bonko24@users.noreply.github.com>
2024-06-12 21:51:43 -0600

    Update connection.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -11,6 +11,17 @@ import { dirname } from 'path';
 const currentDir = dirname(fileURLToPath(import.meta.url));
 
 const dbSettings = {
+    user: "Admin",
+    password: "luis",
+    server: "25.7.30.30",   // Cambiar ip real al momento con samir
+    database: "Proyecto2Bd_2024",
+    options:{
+        encrypt: false,
+        trustServerCertificate: true,
+    }
+}
+/*
+const dbSettings = {
     user: "esteban",
     password: "333",
     server: "localhost",   // Cambiar ip real al momento con samir
@@ -20,7 +31,7 @@ const dbSettings = {
         trustServerCertificate: true,
     }
 }
-
+*/
 
 export const getConnection = async () => {
     try {
@@ -36,7 +47,8 @@ export const scheduleBackup = () => {
     cron.schedule('30 23 * * *', async () => { // Ejecutar a las 10:41 PM todos los días
         try {
             const pool = await sql.connect(dbSettings);
-            const backupPath = `C:\Users\Victor Azofeifa\Downloads\\backup_${new Date().toISOString().replace(/[:.]/g, '-')}.bak`;
+            const backupPath = `C:\\Users\\samir\\Documents\\BackUpSqlProyecto\\backup_${new Date().toISOString().replace(/[:.]/g, '-')}.bak`;
+            //const backupPath = `C:\Users\Victor Azofeifa\Downloads\\backup_${new Date().toISOString().replace(/[:.]/g, '-')}.bak`;
             const query = `BACKUP DATABASE ${dbSettings.database} TO DISK = '${backupPath}'`;
             await pool.request().query(query);
             console.log("Backup realizado con éxito en:", backupPath);

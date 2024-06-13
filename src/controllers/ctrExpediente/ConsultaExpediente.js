09e02af264a09f7b02d521050b6ee33942bd3962
AndyBS1 <andy230505@gmail.com>
2024-06-13 13:24:25 -0600

    Funcion de GetExpedienteByDuegno

--- src/controllers/ctrExpediente/ConsultaExpediente.js
+++ src/controllers/ctrExpediente/ConsultaExpediente.js
@@ -45,3 +45,19 @@ export const getExpedienteByMascota = async (req, res) => {
         res.status(500).send("Error al obtener un solo expediente");
     }
 }
+
+//consultar expedientes segun la mascota
+export const getExpedienteByDuegno = async (req, res) => {
+    try {
+        const { id } = req.params; // Obtener el ID de los parámetros de la URL
+        const pool = await getConnection();
+        const result = await pool.request()
+            .input('IdExpediente', id)
+            .query('exec expedienteConsultaByDuegno @IdExpediente'); // Utilizar una consulta parametrizada
+        // Enviar una respuesta con el resultado de la consulta
+        res.send(result.recordset);
+    } catch (error) {
+        console.error("Error al obtener un solo expediente:", error);
+        res.status(500).send("Error al obtener un solo expediente");
+    }
+}
